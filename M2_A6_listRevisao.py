09030770972432a94a63dcf45345a34166cb6d8a
guilherme <guilhermemarzano@gmail.com>
2023-11-20 19:43:30 -0300

    Adding revision class

---
+++ M2_A6_listRevisao.py
@@ -0,0 +1,17 @@
+# como criar listas 
+# []
+lst_frutas = ['maça','banana','abacate','morango','abacate']
+print(lst_frutas)
+#Adicionar valor a uma lista 
+#Append - lst_frutas.append('mamao')
+
+#Para adicionar uma fruta entre um item
+#insert
+#lst_frutas.insert(1,'maracuja')
+
+#Para pegar um pedaço da lista.
+print(lst_frutas[1:3])
+#Passando o valor -1 ele traz o ultimo valor da lista.
+print(lst_frutas[-1])
+#Para inverter a lista inteira
+print(lst_frutas[::-1])
